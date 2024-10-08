a52190cc68aabbc00ce899b3724f9eeaa07f503a
vincent anjiri <vincentanjiri12@gmail.com>
2024-10-08 11:19:33 +0300

    "Added MainHeader component import and usage, updated metadata title and description quotes, and changed color code quotes in linearGradient stops."

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1>404: Page Not Found</h1>
+    </div>
+  );
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../assets/logo.png";
+import classes from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <Image src={logo} alt="logo image" priority />
+        Nextlevel food
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foddies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
